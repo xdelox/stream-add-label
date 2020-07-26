db9dcc554768ca7271eda8a57b84d7fa374e5a63
Sergio De Lorenzis <sergio@graphaware.com>
2020-07-26 20:22:51 +0200

    Working with Launcher

---
+++ src/main/java/DBService.java
@@ -0,0 +1,58 @@
+
+import org.neo4j.driver.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DBService {
+    final Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+    final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public void addLabelsToNodes() {
+        try (Session session = driver.session()) {
+            session.readTransaction(tx -> {
+                Result result = tx.run("MATCH (a:MyLabel) RETURN ID(a) as identify ORDER BY ID(a)");
+                result
+                        .forEachRemaining((e) ->
+                                executor.submit(() ->
+                                        updateRecord(e.get(0).asInt())
+                                ));
+                return result;
+            });
+
+        }
+    }
+
+    public void fillDatabase(){
+        executeWriteQuery("UNWIND RANGE(1,1000) AS a CREATE(n:MyLabel {value:a}) return n");
+    }
+
+    public void deleteDatabase() {
+        executeWriteQuery("MATCH (n) DETACH DELETE n");
+    }
+
+    private void executeWriteQuery(String query){
+        try (Session session = driver.session()) {
+            session.writeTransaction(tx -> tx.run(query));
+        }
+    }
+
+    private void updateRecord(int id) {
+
+        try (Session session2 = driver.session()) {
+            session2.writeTransaction(txw ->
+            {
+                Map<String, Object> params = new HashMap<>();
+                params.put("id", id);
+                String query = "MATCH (n) where ID(n)=$id set n :NewLabel return n";
+                System.out.println("Updating node with id: " + params.get("id"));
+                final Result result = txw.run(query, params);
+                txw.commit();
+                return result;
+            });
+        }
+
+    }
+}
