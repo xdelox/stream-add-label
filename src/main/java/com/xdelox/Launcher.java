03ec68e673beda4f16e3a1b829c2df88beac1d40
Sergio De Lorenzis <sergio@graphaware.com>
2020-07-27 02:18:14 +0200

    Final refactoring. Test draft.

--- src/main/java/com/xdelox/Launcher.java
+++ src/main/java/com/xdelox/Launcher.java
@@ -1,9 +1,12 @@
 package com.xdelox;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
+
 public class Launcher {
 
     public static void main(String[] args) {
-        final DBService service = new DBService();
+        final DBService service = new DBService(GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password")));
         service.deleteDatabase();
         //service.fillDatabase();
         service.fillDatabase();
